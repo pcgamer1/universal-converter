b142a12b81f67ef47b2d18559a3643f9eef3953c
sarthak_saxena_72 <i.sarthak.saxena@redcarpetup.com>
2021-04-10 01:14:18 +0530

    react build integrated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const converterRouter = require('./src/routers/converterRouter')
 const configRouter = require('./src/routers/configRouter')
+const path = require('path')
 
 const app = express()
 
@@ -9,5 +10,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(converterRouter)
 app.use(configRouter)
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app
